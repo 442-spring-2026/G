208bda22996be3f24ed626122a882740f79d2490
Dylan Song <112503726+dylans0ng@users.noreply.github.com>
2026-05-12 00:30:51 -0700

    Fix duplicate export of db in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,4 +15,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
