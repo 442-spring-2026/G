cf489629ee7620840caf806339b9a6b6f8beed38
David Yang <davidyang@DaviddeMacBook-Pro.local>
2026-05-12 14:34:13 -0700

    fix navigation to dashboard closes #10

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes, Outlet } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+<<<<<<< Updated upstream
 import SavedCabinet from "./pages/SavedCabinet";
 
 function App() {
@@ -12,5 +13,41 @@ function App() {
     </Routes>
   );
 }
+=======
+import AddMedicinePage from "./pages/AddMedicinePage";
+import Navbar from "./navbar";
+import Home from "./pages/Home";
 
-export default App;
+
+// Navbar is only on post-login pages (Requirement N1)
+const AuthenticatedLayout = () => {
+ return (
+   <div style={{ paddingTop: '64px' }}>
+     <Navbar />
+     <Outlet />
+   </div>
+ );
+};
+
+
+function App() {
+ return (
+   <Routes>
+     {/* Login page has NO navbar (Requirement N1) */}
+     <Route path="/login" element={<LoginPage />} />
+>>>>>>> Stashed changes
+
+
+     {/* Pages inside this block WILL have the navbar (Post-Login) */}
+     <Route element={<AuthenticatedLayout />}>
+     <Route path="/home" element={<Home />} />
+       <Route path="/add" element={<AddMedicinePage />} />
+       {/* Requirement N5 */}
+       <Route path="/dashboard" element={<div>Medicine Cabinet Page</div>} />
+       {/* Requirement N7 */}
+       <Route path="/reminders" element={<div>Reminders Page</div>} />
+     </Route>
+
+
+     <Route path="*" element={<Navigate to="/login" replace />} />
+   </Routes>
