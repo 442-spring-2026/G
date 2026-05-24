d313c76d0cc512204ac4c8f3f3994a248a70de53
David Yang <davidyang@daviddembp.mynetworksettings.com>
2026-05-24 13:19:58 -0700

    fix: resolve merge conflict, keep /reminders route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Navigate, Route, Routes, Outlet } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import Home from "./pages/Home";
-import SavedCabinet from "./pages/SavedCabinet";
+import Dashboard from "./pages/Dashboard";
 import AddMedicinePage from "./pages/AddMedicinePage";
 import ManageMedication from "./pages/ManageMedication";
 import Navbar from "./navbar";
@@ -10,13 +12,19 @@ function App() {
   return (
     <div className="app-shell">
       <Routes>
-        {/* Login page has NO navbar (Requirement N1) */}
+        {/* Login & Signup pages have NO navbar (Requirement N1) */}
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+
+        {/* Main pages HAVE navbar */}
         <Route path="/home" element={<><Navbar /><Home /></>} />
-        <Route path="/dashboard" element={<><Navbar /><SavedCabinet /></>} />
+        <Route path="/dashboard" element={<><Navbar /><Dashboard /></>} />
         <Route path="/addmedicinepage" element={<><Navbar /><AddMedicinePage /></>} />
         <Route path="/manage/:id" element={<><Navbar /><ManageMedication /></>} />
-        <Route path="/reminders" element={<><Navbar /><SavedCabinet /></>} />
+        <Route path="/reminders" element={<><Navbar /><Dashboard /></>} />
+
+        {/* Catch-all fallback */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </div>
@@ -24,3 +32,4 @@ function App() {
 }
 
 export default App;
+
