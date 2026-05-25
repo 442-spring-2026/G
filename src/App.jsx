18fcf8d3ef803b2ea6914d4cb6ea66d0603162a1
David Yang <davidyang@DaviddeMacBook-Pro.local>
2026-05-25 16:08:04 -0700

    fix: resolve App.jsx conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import Home from "./pages/Home";
-// import SavedCabinet from "./pages/SavedCabinet";
 import Dashboard from "./pages/Dashboard";
 import AddMedicinePage from "./pages/AddMedicinePage";
 import ManageMedication from "./pages/ManageMedication";
 import Navbar from "./navbar";
-
 
 function App() {
   return (
@@ -17,14 +15,15 @@ function App() {
         {/* Login & Signup pages have NO navbar (Requirement N1) */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgotpassword" element={<ForgotPasswordPage />} /> {/* Fixed string to match login navigate */}
+        <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
 
         {/* Main pages HAVE navbar */}
         <Route path="/home" element={<><Navbar /><Home /></>} />
         <Route path="/dashboard" element={<><Navbar /><Dashboard /></>} />
         <Route path="/addmedicinepage" element={<><Navbar /><AddMedicinePage /></>} />
         <Route path="/manage/:id" element={<><Navbar /><ManageMedication /></>} />
-        
+        <Route path="/reminders" element={<><Navbar /><Dashboard /></>} />
+
         {/* Catch-all fallback */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
@@ -33,3 +32,4 @@ function App() {
 }
 
 export default App;
+
